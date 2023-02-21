37bc924ac17e1fbc4834cec6f2f991f8a2f2d1d4
saranoor <saranoor@live.com>
2023-02-21 17:50:12 +0500

    creating model with mongoose

---
+++ Basic_practice/task-manager/src/db/mongoose.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose')
+mongoose.connect('mongodb://localhost:27017/task-manager-api',{
+})
+
+const taskUser = mongoose.model('taskUser',{
+    description:{
+        type: String,
+        required: true,
+        trim:true
+    },
+    completed:{
+        type: Boolean,
+        default: false
+    }
+})
+
+const firstCollection = new taskUser({
+    description:'eat breakfast'
+})
+
+firstCollection.save().then(()=>{
+    console.log(firstCollection)
+}).catch((error)=>{
+    console.log('Error', error)
+})
+//const me = new User({
+//    name:"Karen",
+//    age:'26oooogfh'
+//})
+//
+//me.save().then(()=>{
+//    console.log(me)
+//}).catch((error)=>{
+//    console.log('Error', error)
+//})
