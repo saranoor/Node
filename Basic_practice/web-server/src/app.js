f67a6608f0532fbf3779c6f37eb9b447d3450e1a
saranoor <saranoor@live.com>
2023-02-21 11:29:40 +0500

    making sure weather endpoint accepts addres in search query

--- Basic_practice/web-server/src/app.js
+++ Basic_practice/web-server/src/app.js
@@ -32,10 +32,25 @@ app.get('/help', (req, res) =>{
 })
 
 app.get('/weather',(req, res)=>{
+    if (!req.query.address){
+        return res.send({
+        error:"you must provide a address"
+        })
+    }
     res.send({
         forecast:"windy",
-        location:"xyz"
+        location:req.query.address
     })
+})
+
+app.get('/products',(req, res)=>{
+    if (!req.query.search){
+        return res.send({
+        error:"you must provide a search term"
+        })
+    }
+    console.log("hey")
+    res.send({product:[]})
 })
 
 app.get('*',(req, res)=>{
