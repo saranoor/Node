097a3d62bf44d0c579b05da1de13d74bd83fce97
saranoor <saranoor@live.com>
2023-02-24 19:23:16 +0500

    challenge for sending socke.io event

--- Basic_practice/chat-app/src/index.js
+++ Basic_practice/chat-app/src/index.js
@@ -12,16 +12,16 @@ const publicDirectoryPath = path.join(__dirname, '../public')
 
 app.use(express.static(publicDirectoryPath))
 
-let count = 0
+//let count = 0
 io.on('connection', (socket) => {
     console.log('New WebSocket connection')
 
-    socket.emit('countUpdated', count)
+    socket.emit('message',"Welcome to server!")
 
-    socket.on('increment', ()=>{
-        count++
-        socket.emit('countUpdated', count)
+    socket.on('sendMessage', (message) => {
+        io.emit('message',message )
     })
+
 })
 
 
