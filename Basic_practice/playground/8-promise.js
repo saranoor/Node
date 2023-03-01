1996fc95bceea904b8b7ab779652378a60ff266f
saranoor <saranoor@live.com>
2023-03-01 13:16:52 +0500

    understanding promise chaining

--- Basic_practice/playground/8-promise.js
+++ Basic_practice/playground/8-promise.js
@@ -1,11 +1,22 @@
-const doWorkPromise = new Promise((resolve, reject)=>{
-    setTimeout(()=>{
-        reject('Things went wrong')
-    },2000)
-})
+const add =(a,b)=>{
+    return new Promise((resolve, reject)=>{
+//        resolve(a+b)
+          reject("error")
+    })
+    }
+// this code calls function add and deals with the output of promise
+//add(1,2).then((sum)=>{
+//    console.log(sum)
+//}).catch((e)=>{
+//    console.log("error")
+//})
 
-doWorkPromise.then((result)=>{
-    console.log('succes', result)
-}).catch((error)=>{
-    console.log('Error', error)
+// lets suppose we need to call get the sum returned from the add and then make another add call
+
+add(1,2).then((sum)=>{
+    return add(sum,1)
+}).then((sum2)=>{
+    console.log(sum2)
+}).catch((e)=>{
+    console.log("error")
 })
