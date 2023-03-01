1de4d5f1ce2e3d0b739812a4ea10bd2e13564aeb
saranoor <saranoor@live.com>
2023-03-01 12:53:17 +0500

    understandign callback vs promises

--- Basic_practice/playground/4-callbacks.js
+++ Basic_practice/playground/4-callbacks.js
@@ -1,7 +1,23 @@
-const geocode =(address, callback)=>{
-     callback(address)
-}
+//const geocode =(address, callback)=>{
+//     callback('undefined',address)
+//}
+//
+//geocode('philli',(err, res)=>{
+//    if(err){
+//        return console.log("can't print")
+//    }
+//    console.log(res)
+//
+//})
+//
+//// we will do the same code with callbakc, in a way promise will replace callback
+// - ac to my understanding so far, you can't pass an argument to a constructor
+// this is a promise not a funciton; it can be called as promise constructor function
+// so geocode is a constant not a function unlike above
+const geocode= new Promise((resolve, reject)=>{
+    resolve('abc')
+})
 
-geocode('philli',(res)=>{
-  console.log(res)
+geocode.then((res)=>{
+    console.log(res)
 })
